c541d08d31452a22de3f5a004014db33b07d89fc
gdlyeabkov <gdlyeabkov@gmail.com>
2022-03-13 18:57:25 +0300

    add_output_access_points

---
+++ Driver/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using SimpleWifi;
+using System.Speech.Synthesis;
+
+namespace Driver
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        public SpeechSynthesizer debugger;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Initialize();
+
+        }
+
+        public void Initialize()
+        {
+            debugger = new SpeechSynthesizer();
+            Wifi wifi = new Wifi();
+            IEnumerable<AccessPoint> accessPoints = wifi.GetAccessPoints();
+            foreach (AccessPoint accessPoint in accessPoints)
+            {
+                String wifiConnectionName = accessPoint.Name;
+                // debugger.Speak(@"Название: " + wifiConnectionName);
+                StackPanel wifiConnection = new StackPanel();
+                wifiConnection.Orientation = Orientation.Horizontal;
+                TextBlock wifiConnectionNameLabel = new TextBlock();
+                wifiConnectionNameLabel.Text = wifiConnectionName;
+                wifiConnection.Children.Add(wifiConnectionNameLabel);
+                wifiConnections.Children.Add(wifiConnection);
+            }
+        }
+
+    }
+}
